e971e82fd4bf3523f8e23f887571edff68bd91a7
SERALBLA <83030522+SERALBLA@users.noreply.github.com>
2021-09-01 20:23:55 +0200

    Add files via upload

---
+++ lampara.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0){
+            $("header").addClass("header2");
+        }else{
+            $("header").removeClass("header2");
+        }
+    });
+});
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-788R0DC0RB');
+
+var btn = document.getElementById("btn");
+
+function Funcion(){
+location.href = "lamparas-de-escritorio.html";
+}
+function Funcion2(){
+    location.href = "lamparas-de-techo.html";
+}
+function Funcion3(){
+    location.href = "lamparas-de-araña.html";
+}
+function Funcion4(){
+    location.href = "lamparas-de-pie.html";
+}
+function Funcion5(){
+    location.href = "lamparas-de-pared.html";
+}
+function Funcion6(){
+    location.href = "lamparas-de-volcan.html";
+}
+function Funcion9(){
+    location.href = "comentarios.html";
+}
+
+
+
